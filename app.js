784107dbbb8cc4991c091e6f8677a5b8a9019944
suleyman3206 <suleymanbarisdalli@gmail.com>
2023-02-04 12:40:00 +0300

    website

---
+++ app.js
@@ -0,0 +1,97 @@
+
+function goster1(e){
+    const hey=document.querySelector(".navic1");
+    hey.style.display="flex";
+    
+}
+
+function sakla1(){
+    const hey=document.querySelector(".navic1");
+    hey.style.display = "none";
+
+}
+function goster2(e){
+    const hey=document.querySelector(".navic2");
+    hey.style.display="flex";
+   
+}
+
+function sakla2(){
+   
+    const hey=document.querySelector(".navic2");
+    hey.style.display = "none";
+
+}
+function goster3(e){
+    const hey=document.querySelector(".navic3");
+    hey.style.display="flex";
+    
+}
+
+function sakla3(){
+    console.log("selams");
+    const hey=document.querySelector(".navic3");
+    hey.style.display = "none";
+
+}
+function goster4(e){
+    const hey=document.querySelector(".navic4");
+    hey.style.display="flex";
+   
+}
+
+function sakla4(){
+    
+    const hey=document.querySelector(".navic4");
+    hey.style.display = "none";
+
+}
+function goster5(e){
+    const hey=document.querySelector(".navic5");
+    hey.style.display="flex";
+    
+}
+
+function sakla5(){
+   
+    const hey=document.querySelector(".navic5");
+    hey.style.display = "none";
+
+}
+function goster6(e){
+    const hey=document.querySelector(".navic6");
+    hey.style.display="flex";
+    
+}
+
+function sakla6(){
+  
+    const hey=document.querySelector(".navic6");
+    hey.style.display = "none";
+
+}
+function goster7(e){
+    const hey=document.querySelector(".navic7");
+    hey.style.display="flex";
+    
+}
+
+function sakla7(){
+  
+    const hey=document.querySelector(".navic7");
+    hey.style.display = "none";
+
+}
+function goster8(e){
+    const hey=document.querySelector(".navic8");
+    hey.style.display="flex";
+   
+}
+
+function sakla8(){
+    
+    const hey=document.querySelector(".navic8");
+    hey.style.display = "none";
+
+}
+
